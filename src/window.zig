4cb45f1b886f2274b5d2dfcb2888725b40ed9826
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-01-20 10:26:00 -0500

    Window - added `set_transient_for()` method Replaced references to `Window` with `Self` where appropriate

--- src/window.zig
+++ src/window.zig
@@ -42,42 +42,51 @@ pub const ApplicationWindow = struct {
 pub const Window = struct {
     ptr: *c.GtkWindow,
 
-    pub fn new(window_type: WindowType) Window {
-        return Window{
+    const Self = @This();
+
+    pub fn new(window_type: WindowType) Self {
+        return Self{
             .ptr = @ptrCast(*c.GtkWindow, c.gtk_window_new(window_type.parse())),
         };
     }
 
-    pub fn set_title(self: Window, title: [:0]const u8) void {
+    pub fn set_title(self: Self, title: [:0]const u8) void {
         c.gtk_window_set_title(self.ptr, title);
     }
 
-    pub fn set_default_size(self: Window, hsize: c_int, vsize: c_int) void {
+    pub fn set_default_size(self: Self, hsize: c_int, vsize: c_int) void {
         c.gtk_window_set_default_size(self.ptr, hsize, vsize);
     }
 
-    pub fn set_decorated(self: Window, decorated: bool) void {
+    pub fn set_decorated(self: Self, decorated: bool) void {
         const val = bool_to_c_int(decorated);
         c.gtk_window_set_decorated(self.ptr, val);
     }
 
-    pub fn close(self: Window) void {
+    pub fn close(self: Self) void {
         c.gtk_window_close(self.ptr);
     }
 
-    pub fn as_container(self: Window) Container {
+    pub fn set_transient_for(self: Self, parent: Self) void {
+        c.gtk_window_set_transient_for(self.ptr, parent.ptr);
+    }
+
+    pub fn as_container(self: Self) Container {
         return Container{
             .ptr = @ptrCast(*c.GtkContainer, self.ptr),
         };
     }
 
-    pub fn as_widget(self: Window) Widget {
+    pub fn as_widget(self: Self) Widget {
         return Widget{
             .ptr = @ptrCast(*c.GtkWidget, self.ptr),
         };
     }
 
     pub fn is_instance(gtype: u64) bool {
-        return (gtype == c.gtk_window_get_type() or ApplicationWindow.is_instance(gtype) or Dialog.is_instance(gtype));
+        return (gtype == c.gtk_window_get_type()
+            or ApplicationWindow.is_instance(gtype)
+            or Dialog.is_instance(gtype)
+        );
     }
 };
