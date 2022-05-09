43055fbfb561ad045e72cdf9bab1948d611a785f
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-05-09 18:35:49 -0400

    Add signal handler methods and some doc comments

--- src/vte.zig
+++ src/vte.zig
@@ -6,62 +6,107 @@ const std = @import("std");
 const fmt = std.fmt;
 const mem = std.mem;
 
-/// PtyFlags enum
-pub const PtyFlags = enum(c_uint) {
-    no_lastlog = c.VTE_PTY_NO_LASTLOG,
-    no_utmp = c.VTE_PTY_NO_UTMP,
-    no_wtmp = c.VTE_PTY_NO_WTMP,
-    no_helper = c.VTE_PTY_NO_HELPER,
-    no_fallback = c.VTE_PTY_NO_FALLBACK,
-    default = c.VTE_PTY_DEFAULT,
+/// An enumeration type that can be used to specify how the terminal uses extra
+/// allocated space.
+pub const Align = enum(c_uint) {
+    /// align to left/top
+    start = c.VTE_ALIGN_START,
+    /// align to centre
+    center = c.VTE_ALIGN_CENTER,
+    /// align to right/bottom
+    end = c.VTE_ALIGN_END,
 };
 
-/// Zig enum CursorBlinkMode
+/// An enumerated type which can be used to indicate the cursor blink mode for
+/// the terminal.
 pub const CursorBlinkMode = enum(c_uint) {
+    /// Follow GTK+ settings for cursor blinking.
     system = c.VTE_CURSOR_BLINK_SYSTEM,
+    /// Cursor blinks.
     on = c.VTE_CURSOR_BLINK_ON,
+    /// Cursor does not blink.
     off = c.VTE_CURSOR_BLINK_OFF,
 };
 
-/// Zig enum CursorShape
+/// An enumerated type which can be used to indicate what should the terminal
+/// draw at the cursor position.
 pub const CursorShape = enum(c_uint) {
+    /// Draw a block cursor. This is the default.
     block = c.VTE_CURSOR_SHAPE_BLOCK,
+    /// Draw a vertical bar on the left side of character. This is similar to
+    /// the default cursor for other GTK+ widgets.
     ibeam = c.VTE_CURSOR_SHAPE_IBEAM,
+    /// Draw a horizontal bar below the character.
     underline = c.VTE_CURSOR_SHAPE_UNDERLINE,
 };
 
-/// enum Format
-pub const Format = enum(c_uint) {
-    text = c.VTE_FORMAT_TEXT,
-    html = c.VTE_FORMAT_HTML,
-};
-
-/// enum TextBlinkMode
-pub const TextBlinkMode = enum(c_uint) {
-    never = c.VTE_TEXT_BLINK_NEVER,
-    focused = c.VTE_TEXT_BLINK_FOCUSED,
-    unfocused = c.VTE_TEXT_BLINK_UNFOCUSED,
-    always = c.VTE_TEXT_BLINK_ALWAYS,
-};
-
-/// enum EraseBinding
+/// An enumerated type which can be used to indicate which string the terminal
+/// should send to an application when the user presses the Delete or Backspace
+/// keys.
 pub const EraseBinding = enum(c_uint) {
+    /// For backspace, attempt to determine the right value from the terminal's
+    /// IO settings. For delete, use the control sequence.
     auto = c.VTE_ERASE_AUTO,
+    /// Send an ASCII backspace character (0x08).
     ascii_backspace = c.VTE_ERASE_ASCII_BACKSPACE,
+    /// Send an ASCII delete character (0x7F).
     ascii_delete = c.VTE_ERASE_ASCII_DELETE,
+    /// Send the "@7 " control sequence.
     delete_sequence = c.VTE_ERASE_DELETE_SEQUENCE,
+    /// Send terminal's "erase" setting.
     tty = c.VTE_ERRASE_TTY,
 };
 
-pub const CursorPosition = struct {
-    x: c_long,
-    y: c_long,
+/// An enumerated type which can be used to indicate whether the terminal allows
+/// the text contents to be blinked.
+pub const TextBlinkMode = enum(c_uint) {
+    /// Do not blink the text.
+    never = c.VTE_TEXT_BLINK_NEVER,
+    /// Allow blinking text only if the terminal is focused.
+    focused = c.VTE_TEXT_BLINK_FOCUSED,
+    /// Allow blinking text only if the terminal is unfocused.
+    unfocused = c.VTE_TEXT_BLINK_UNFOCUSED,
+    /// Allow blinking text. This is the default.
+    always = c.VTE_TEXT_BLINK_ALWAYS,
+};
+
+/// An enumeration type that can be used to specify the format the selection
+/// should be copied to the clipboard in.
+pub const Format = enum(c_uint) {
+    /// Export as plain text
+    text = c.VTE_FORMAT_TEXT,
+    /// Export as HTML formatted text
+    html = c.VTE_FORMAT_HTML,
+};
+
+/// A flag type to determine how terminal contents should be written to an
+/// output stream.
+pub const WriteFlags = enum(c_uint) {
+    default = c.VTE_WRITE_DEFAULT,
+};
+
+/// An enumeration type for features.
+pub const FeatureFlags = enum(c_uint) {
+    /// whether VTE was built with bidirectional text support
+    bidi = c.VTE_FEATURE_FLAG_BIDI,
+    /// whether VTE was built with ICU support
+    icu = c.VTE_FEATURE_FLAG_ICU,
+    /// whether VTE was built with systemd support
+    systemd = c.VTE_FEATURE_FLAG_SYSTEMD,
+    /// whether VTE was built with SIXEL support
+    sixel = c.VTE_FEATURE_FLAG_SIXEL,
+    /// mask of all feature flags
+    mask = c.VTE_FEATURE_FLAGS_MASK,
 };
 
 pub const Terminal = struct {
     ptr: *c.VteTerminal,
 
     const Self = @This();
+    pub const CursorPosition = struct {
+        x: c_long,
+        y: c_long,
+    };
 
     pub fn new() Self {
         return Self{
@@ -379,8 +424,76 @@ pub const Terminal = struct {
         } else return null;
     }
 
+    pub fn connect_bell(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("bell", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_char_size_changed(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("char-size-changed", callback, if (data) |d| d else null);
+    }
+
     pub fn connect_child_exited(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
         self.as_widget().connect("child_exited", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_commit(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("commit", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_contents_changed(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("contents-changed", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_copy_clipboard(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("copy-clipboard", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_current_directory_uri_changed(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("current-directory-uri-changed", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_current_file_uri_changed(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("current-file-uri-changed", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_cursor_moved(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("cursor-moved", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_decrease_font_size(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("decreas-font-size", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_encoding_changed(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("encoding-changed", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_eof(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("eof", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_hyperlink_hover_uri_changedl(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("hyperlink-hover-usi-changed", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_increas_font_size(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("increase-font-size", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_paste_clipboard(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("paste-clipboard", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_resize_window(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("resize-window", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_selection_changed(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("selection-changed", callback, if (data) |d| d else null);
+    }
+
+    pub fn connect_window_title_changed(self: Terminal, callback: c.GCallback, data: ?c.gpointer) void {
+        self.as_widget().connect("window-title-changed", callback, if (data) |d| d else null);
     }
 
     pub fn as_widget(self: Terminal) Widget {
@@ -394,4 +507,14 @@ pub const Terminal = struct {
             .ptr = @ptrCast(*c.VteTerminal, widget.ptr),
         };
     }
+};
+
+/// PtyFlags enum
+pub const PtyFlags = enum(c_uint) {
+    no_lastlog = c.VTE_PTY_NO_LASTLOG,
+    no_utmp = c.VTE_PTY_NO_UTMP,
+    no_wtmp = c.VTE_PTY_NO_WTMP,
+    no_helper = c.VTE_PTY_NO_HELPER,
+    no_fallback = c.VTE_PTY_NO_FALLBACK,
+    default = c.VTE_PTY_DEFAULT,
 };
